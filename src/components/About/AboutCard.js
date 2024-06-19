c8f920f8d4452bebf79bae58887ccb2f3ea45b05
Matheus Hentges <matheus-hentges@live.com>
2024-06-19 00:33:34 +0000

    second commit

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,16 +8,15 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Matheus Hentges </span>
+            from <span className="purple"> Chapecó, Brasil.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            Sou desenvolvedor freelancer há 1 ano.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            Sou formado em análise e desenvolvimento de sistemas - 2023
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            Entusiasmado e apaixonado pelo mundo da tecnologia!
           </p>
           <ul>
             <li className="about-activity">
